19f387ff6924355e0a7128d5f4d074681ff5ad88
medinat akinsowon <medinahakinsowon@gmail.com>
2023-08-05 14:59:37 +0100

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/App.css'
